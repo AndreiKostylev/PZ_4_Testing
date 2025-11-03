e8dfbd1bf871faa0ded1abb8accb385c63a96cce
Андреев Андрей <kostandr06@icloud.com>
2025-11-03 13:53:58 +0500

    refactor: декомпозиция метода AnalyzeText - Выделен CountWords() для подсчета слов - Создан FindLongestWord() для поиска длинного слова - Добавлен CalculateCharacterFrequency() для анализа символов - Упрощена тестируемость компонентов

---
+++ Program.cs
@@ -0,0 +1,23 @@
+[33mcommit de77c44d1ada1edf18a81afe1acfd67040a02dcf[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mrefactoring[m[33m, [m[1;31morigin/refactoring[m[33m)[m
+Author: Андреев Андрей <kostandr06@icloud.com>
+Date:   Mon Nov 3 11:52:15 2025 +0500
+
+    resolve merge conflicts
+
+[33mcommit 80115e47982f206113d6f7f5fd03b42677379ea0[m
+Author: Андреев Андрей <kostandr06@icloud.com>
+Date:   Mon Nov 3 11:32:46 2025 +0500
+
+    добавление comprehensive README.md с анализом легаси кода
+
+[33mcommit 46bce71063a8a99aee310f9d9b928d3a3619a1b3[m[33m ([m[1;31morigin/master[m[33m, [m[1;31morigin/HEAD[m[33m, [m[1;32mmaster[m[33m)[m
+Author: nerobeev.an2215 <nerobeev.an2215@PC-5-ROOM313>
+Date:   Thu Oct 30 14:33:14 2025 +0300
+
+    Добавьте файлы проекта.
+
+[33mcommit 37358d250ee4dcfda823790c66ab73150f347bf9[m
+Author: nerobeev.an2215 <nerobeev.an2215@PC-5-ROOM313>
+Date:   Thu Oct 30 14:33:12 2025 +0300
+
+    Добавить .gitattributes и .gitignore.
